dfab651498826fd58675a67bc930be1128597764
A.Sweilam <anhsbio@gmail.com>
2020-09-01 22:20:54 +0100

    Added integration test file for the UserController with test method on getAllLondon

---
+++ src/test/java/com/example/londonapi/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.example.londonapi.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getAllLondonIntTest() throws Exception {
+        ResponseEntity<List> response =
+                this.restTemplate.getForEntity("http://localhost:" + port + "/api/london", List.class);
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+
+    }
+
+
+
+
+
+}
